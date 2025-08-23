0ce550f159c41a7e7d04d0ec6d7123c0fe1f5868
Lucius Holzinger <luciholzinger@gmail.com>
2025-08-23 14:38:13 -0700

    the prices tags under the header are in to show change in the chart. hardcoded values in only
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- src/components/RevenueExpenseCard.jsx
+++ src/components/RevenueExpenseCard.jsx
@@ -8,8 +8,11 @@ import {
   Icon,
   Portal,
   Box,
+  IconButton,
 } from "@chakra-ui/react";
 import {
+  ArrowDown,
+  ArrowUp,
   CalendarDays,
   ChevronDown,
   ClipboardPaste,
@@ -27,44 +30,80 @@ const RevenueExpenseCard = () => {
       minW={"1/3"}
       rounded={"xl"}
     >
-      <Card.Header>
-        <Flex alignItems={"cener"} justifyContent={"space-between"}>
-          <Card.Title color={"black"}>Analytics</Card.Title>
-          <Menu.Root>
-            <Menu.Trigger asChild>
-              <Button>
-                <CalendarDays />
-                19 Aug - 25 Aug
-                <Icon>
-                  <ChevronDown />
-                </Icon>
-              </Button>
-            </Menu.Trigger>
-            <Portal>
-              <Menu.Positioner>
-                <Menu.Content>
-                  <Menu.Item>
-                    <Scissors />
-                    <Box>Cut</Box>
-                    <Menu.ItemCommand>⌘X</Menu.ItemCommand>
-                  </Menu.Item>
-                  <Menu.Item>
-                    <Copy />
-                    <Box>Copy</Box>
-                    <Menu.ItemCommand>⌘C</Menu.ItemCommand>
-                  </Menu.Item>
-                  <Menu.Item>
-                    <ClipboardPaste />
-                    <Box>Pase</Box>
-                    <Menu.ItemCommand>⌘V</Menu.ItemCommand>
-                  </Menu.Item>
-                </Menu.Content>
-              </Menu.Positioner>
-            </Portal>
-          </Menu.Root>
-        </Flex>
-      </Card.Header>
-      <Card.Body />
+      <Card.Root
+        bg={"white"}
+        rounded={"md"}
+        m={6}
+        variant={"subtle"}
+        border={"2px solid"}
+        borderColor={"gray.200"}
+      >
+        <Card.Header>
+          <Flex alignItems={"center"} justifyContent={"space-between"}>
+            <Card.Title color={"black"}>Analytics</Card.Title>
+            <Menu.Root>
+              <Menu.Trigger asChild>
+                <Button>
+                  <CalendarDays />
+                  19 Aug - 25 Aug
+                  <Icon>
+                    <ChevronDown />
+                  </Icon>
+                </Button>
+              </Menu.Trigger>
+              <Portal>
+                <Menu.Positioner>
+                  <Menu.Content>
+                    <Menu.Item>
+                      <Scissors />
+                      <Box>Cut</Box>
+                      <Menu.ItemCommand>⌘X</Menu.ItemCommand>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <Copy />
+                      <Box>Copy</Box>
+                      <Menu.ItemCommand>⌘C</Menu.ItemCommand>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <ClipboardPaste />
+                      <Box>Pase</Box>
+                      <Menu.ItemCommand>⌘V</Menu.ItemCommand>
+                    </Menu.Item>
+                  </Menu.Content>
+                </Menu.Positioner>
+              </Portal>
+            </Menu.Root>
+          </Flex>
+        </Card.Header>
+        <Card.Body>
+          <Flex gap={12}>
+            <Flex alignItems={"center"} gap={3}>
+              <IconButton
+                bg={"#d5e7ec71"}
+                pointerEvents={"none"}
+                rounded={"xl"}
+              >
+                <ArrowUp color="#328299" />
+              </IconButton>
+              <Text color={"black"} textStyle={"lg"}>
+                $5.890
+              </Text>
+            </Flex>
+            <Flex alignItems={"center"} gap={3}>
+              <IconButton
+                bg={"#ecf3e79c"}
+                pointerEvents={"none"}
+                rounded={"xl"}
+              >
+                <ArrowDown color="#b6eb8e" />
+              </IconButton>
+              <Text color={"black"} textStyle={"lg"}>
+                $1.750
+              </Text>
+            </Flex>
+          </Flex>
+        </Card.Body>
+      </Card.Root>
       <Card.Footer alignItems={"center"} justifyContent={"center"}>
         <Stack alignItems={"center"}>
           <Text color={"black"} textStyle={"2xl"}>

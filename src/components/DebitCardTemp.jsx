7b8f2a08229bba4fbcce1cd57f801b5f9e4f9942
Lucius Holzinger <luciholzinger@gmail.com>
2025-08-25 16:08:33 -0700

    the debit card design for the personalize card card is done
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- src/components/DebitCardTemp.jsx
+++ src/components/DebitCardTemp.jsx
@@ -1,9 +1,75 @@
-import { Button, Theme } from "@chakra-ui/react";
+import { Card, Flex, Image, Theme, Text, Stack } from "@chakra-ui/react";
+import { Circle } from "lucide-react";
 
 const DebitCardTemp = () => {
+  const nums = [1, 2, 3, 4];
+  const numOfSeries = [1, 2, 3];
+
   return (
     <Theme appearance="light">
-      <Button>Click me!</Button>
+      <Card.Root
+        position={"relative"}
+        overflow={"hidden"}
+        bg={"#328299"}
+        minW={"xs"}
+      >
+        <Circle
+          style={{
+            position: "absolute",
+            transform: "translate(55%, 10%)",
+          }}
+          size={384}
+          color="#B6EB8E"
+          fill="#B6EB9E"
+        />
+        <Stack position={"relative"}>
+          <Card.Header>
+            <Flex alignItems={"baseline"} justifyContent={"space-between"}>
+              <Stack gap={-1}>
+                <Theme bg={"#328299"}>
+                  <Text>Current balance</Text>
+                  <Text textStyle={"4xl"} fontWeight={"semibold"}>
+                    70,700.00
+                  </Text>
+                </Theme>
+              </Stack>
+              <Image
+                src="/Visa_Logo.png"
+                alt="visa logo"
+                htmlHeight={"25px"}
+                htmlWidth={"75px"}
+              />
+            </Flex>
+          </Card.Header>
+          <Card.Body></Card.Body>
+          <Card.Footer>
+            <Theme bg={"transparent"}>
+              <Stack minW={"xs"}>
+                <Text>Abdur Rahman</Text>
+                <Flex
+                  gap={18}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                >
+                  <Flex gap={4} alignItems={"center"}>
+                    {numOfSeries.map(() => (
+                      <Flex gap={0.5}>
+                        {nums.map(() => (
+                          <Circle size={3} color={"white"} fill="white" />
+                        ))}
+                      </Flex>
+                    ))}
+                    <Text>87276</Text>
+                  </Flex>
+                  <Flex>
+                    <Text>12/19</Text>
+                  </Flex>
+                </Flex>
+              </Stack>
+            </Theme>
+          </Card.Footer>
+        </Stack>
+      </Card.Root>
     </Theme>
   );
 };

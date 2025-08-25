2f4509af9bce204478e6636bc6a233af153a5b0a
Lucius Holzinger <luciholzinger@gmail.com>
2025-08-25 13:44:38 -0700

    basic button on the screen
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

---
+++ src/components/DebitCardTemp.jsx
@@ -0,0 +1,11 @@
+import { Button, Theme } from "@chakra-ui/react";
+
+const DebitCardTemp = () => {
+  return (
+    <Theme appearance="light">
+      <Button>Click me!</Button>
+    </Theme>
+  );
+};
+
+export default DebitCardTemp;

fb7561c7b9bcb193c6f52a3f0fae4cc4694ca175
Lucius Holzinger <luciholzinger@gmail.com>
2025-08-26 22:29:42 -0700

    the footer and the what makes us different sections are implemented and styled faithful to the design
    Signed-off-by: Lucius Holzinger <luciholzinger@gmail.com>

--- src/components/MakesUnique.jsx
+++ src/components/MakesUnique.jsx
@@ -1,4 +1,12 @@
-import { IconButton, Image, Stack, Table, Text, Theme } from "@chakra-ui/react";
+import {
+  Flex,
+  IconButton,
+  Image,
+  Stack,
+  Table,
+  Text,
+  Theme,
+} from "@chakra-ui/react";
 import { BarChart, LineChart, Lock, Timer, Unlink, Wrench } from "lucide-react";
 
 const MakesUnique = () => {
@@ -9,97 +17,130 @@ const MakesUnique = () => {
 
   return (
     <Theme appearance="light">
-      <Stack>
-        <Text>What Makes Dashvibe Different?</Text>
-        <Text>
-          Transform the Way You Manage Finances with These Key Advantages and
-          Powerful Features
-        </Text>
-        <Image
-          src="make-different-screen.png"
-          alt="screen for uhhh..."
-          rounded={"2xl"}
-        />
-        <Table.Root size="sm" showColumnBorder>
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>
-                <IconButton pointerEvents={"none"}>
-                  <Unlink />
-                </IconButton>
-                <Text textStyle={"xl"} fontWeight={"bold"}>
-                  Effortless Connectivity
-                </Text>
-                <Text textStyle={"md"}>
-                  Easily connect Dashvibe with your current ERP, CRM, or
-                  accounting tools
-                </Text>
-              </Table.Cell>
-              <Table.Cell>
-                <IconButton pointerEvents={"none"}>
-                  <LineChart />
-                </IconButton>
-                <Text textStyle={"xl"} fontWeight={"bold"}>
-                  Effortless Connectivity
-                </Text>
-                <Text textStyle={"md"}>
-                  Easily connect Dashvibe with your current ERP, CRM, or
-                  accounting tools
-                </Text>
-              </Table.Cell>
-              <Table.Cell>
-                <IconButton pointerEvents={"none"}>
-                  <Wrench />
-                </IconButton>
-                <Text textStyle={"xl"} fontWeight={"bold"}>
-                  Effortless Connectivity
-                </Text>
-                <Text textStyle={"md"}>
-                  Easily connect Dashvibe with your current ERP, CRM, or
-                  accounting tools
-                </Text>
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>
-                <IconButton pointerEvents={"none"}>
-                  <Timer />
-                </IconButton>
-                <Text textStyle={"xl"} fontWeight={"bold"}>
-                  Effortless Connectivity
-                </Text>
-                <Text textStyle={"md"}>
-                  Easily connect Dashvibe with your current ERP, CRM, or
-                  accounting tools
-                </Text>
-              </Table.Cell>
-              <Table.Cell>
-                <IconButton pointerEvents={"none"}>
-                  <BarChart />
-                </IconButton>
-                <Text textStyle={"xl"} fontWeight={"bold"}>
-                  Effortless Connectivity
-                </Text>
-                <Text textStyle={"md"}>
-                  Easily connect Dashvibe with your current ERP, CRM, or
-                  accounting tools
-                </Text>
-              </Table.Cell>
-              <Table.Cell>
-                <IconButton pointerEvents={"none"}>
-                  <Lock />
-                </IconButton>
-                <Text textStyle={"xl"} fontWeight={"bold"}>
-                  Effortless Connectivity
-                </Text>
-                <Text textStyle={"md"}>
-                  Easily connect Dashvibe with your current ERP, CRM, or
-                  accounting tools
-                </Text>
-              </Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table.Root>
+      <Stack alignItems={"center"}>
+        <Stack alignItems={"center"} gap={20}>
+          <Stack alignItems={"center"} gap={3}>
+            <Text fontWeight={"semibold"} textStyle={"4xl"} color={"black"}>
+              What Makes Dashvibe Different?
+            </Text>
+            <Text textStyle={"lg"} textAlign={"center"} color={"gray.500"}>
+              Transform the Way You Manage Finances with These Key Advantages
+              and Powerful Features
+            </Text>
+          </Stack>
+          <Image
+            src="make-different-screen.png"
+            alt="screen for uhhh..."
+            rounded={"2xl"}
+            width={1400}
+            height={700}
+          />
+        </Stack>
+
+        <Flex maxW={"5/6"}>
+          <Table.Root size="lg" showColumnBorder>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>
+                  <IconButton
+                    pointerEvents={"none"}
+                    bg={"gray.100"}
+                    color={"black"}
+                  >
+                    <Unlink />
+                  </IconButton>
+                  <Text textStyle={"xl"} fontWeight={"bold"}>
+                    Effortless Connectivity
+                  </Text>
+                  <Text textStyle={"md"}>
+                    Easily connect Dashvibe with your current ERP, CRM, or
+                    accounting tools
+                  </Text>
+                </Table.Cell>
+                <Table.Cell>
+                  <IconButton
+                    pointerEvents={"none"}
+                    bg={"gray.100"}
+                    color={"black"}
+                  >
+                    <LineChart />
+                  </IconButton>
+                  <Text textStyle={"xl"} fontWeight={"bold"}>
+                    Built to grow
+                  </Text>
+                  <Text textStyle={"md"}>
+                    Dashvibe scales with you, from startups to large enterprises
+                  </Text>
+                </Table.Cell>
+                <Table.Cell>
+                  <IconButton
+                    pointerEvents={"none"}
+                    bg={"gray.100"}
+                    color={"black"}
+                  >
+                    <Wrench />
+                  </IconButton>
+                  <Text textStyle={"xl"} fontWeight={"bold"}>
+                    Tailored and Adaptable Solutions
+                  </Text>
+                  <Text textStyle={"md"}>
+                    Customize reports, alerts, and more to match your precise
+                    financial needs
+                  </Text>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  <IconButton
+                    pointerEvents={"none"}
+                    bg={"gray.100"}
+                    color={"black"}
+                  >
+                    <Timer />
+                  </IconButton>
+                  <Text textStyle={"xl"} fontWeight={"bold"}>
+                    Instant analytics
+                  </Text>
+                  <Text textStyle={"md"}>
+                    Make quicker decisions using real-time analytics and trends.
+                  </Text>
+                </Table.Cell>
+                <Table.Cell>
+                  <IconButton
+                    pointerEvents={"none"}
+                    bg={"gray.100"}
+                    color={"black"}
+                  >
+                    <BarChart />
+                  </IconButton>
+                  <Text textStyle={"xl"} fontWeight={"bold"}>
+                    Easy Administration
+                  </Text>
+                  <Text textStyle={"md"}>
+                    Access all your finanical data in one dashboard no more
+                    switching platforms
+                  </Text>
+                </Table.Cell>
+                <Table.Cell>
+                  <IconButton
+                    pointerEvents={"none"}
+                    bg={"gray.100"}
+                    color={"black"}
+                  >
+                    <Lock />
+                  </IconButton>
+                  <Text textStyle={"xl"} fontWeight={"bold"}>
+                    Advanced Protection Solutions
+                  </Text>
+                  <Text textStyle={"md"}>
+                    Your data stays protected with encryption and multi-factor
+                    authentication.
+                  </Text>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table.Root>
+        </Flex>
       </Stack>
     </Theme>
   );
